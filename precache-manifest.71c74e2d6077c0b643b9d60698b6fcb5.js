7f26f744a15f7b23cc341040d22c9cc9a760d2d9
Anthony Melean <brogkul@gmail.com>
2020-09-25 19:29:58 -0400

    Updates

---
+++ precache-manifest.71c74e2d6077c0b643b9d60698b6fcb5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceb96ee6b9e68e18b8ab65afd8af72a7",
+    "url": "/monsterfriends/index.html"
+  },
+  {
+    "revision": "ace8f09afdc9ff4b2cbe",
+    "url": "/monsterfriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "2c3518049793d6c95f75",
+    "url": "/monsterfriends/static/css/main.c16a18ed.chunk.css"
+  },
+  {
+    "revision": "ace8f09afdc9ff4b2cbe",
+    "url": "/monsterfriends/static/js/2.23e0c026.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsterfriends/static/js/2.23e0c026.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c3518049793d6c95f75",
+    "url": "/monsterfriends/static/js/main.36c47004.chunk.js"
+  },
+  {
+    "revision": "4771630902bd6d33b785",
+    "url": "/monsterfriends/static/js/runtime-main.2a12f1c4.js"
+  },
+  {
+    "revision": "abcb7b95ebbaa59d11b9d0e4f5d6f353",
+    "url": "/monsterfriends/static/media/ScaryVampire.abcb7b95.woff"
+  }
+]);
